cfbb1cbffcb9979baceef05c9cea23486341b39a
montaltomarco <montaltomarco0@gmail.com>
2016-06-17 14:43:04 +0200

    tests modified

--- _other/tests/send-notif.js
+++ _other/tests/send-notif.js
@@ -2,7 +2,7 @@ var apn = require('apn');
 
 var options = { cert : '../lovebip_certs/cert_dev.pem', key: '../lovebip_certs/key_dev.pem' };
 var apnConnection = new apn.Connection(options);
-var marcoDevice = new apn.Device("1ef73701615e2fdc36260eb4bf99054813e1a8f377e6368a7f23666f868b5188");//fe36f3fbb4130c9609c9abf2f78ac67057461f5fba7efb67fffedacf2bfe66e9");
+var marcoDevice = new apn.Device("b50e7f19d07999b18e75e94bf270eb241c9e1e663bf07ec4fe4964f23ca9fd56");//fe36f3fbb4130c9609c9abf2f78ac67057461f5fba7efb67fffedacf2bfe66e9");
 
 var noteforMarco = new apn.Notification();
 
